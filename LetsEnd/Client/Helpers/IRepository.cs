e5fccf0e00bb90c3a61904a0f68996ab913abda3
Husnain-Akbar <husnainakbar140@gmail.com>
2020-07-05 17:12:41 +0500

    Add project files.

---
+++ LetsEnd/Client/Helpers/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using LetsEnd.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsEnd.Client.Helpers
+{
+     public interface IRepository
+    {
+        List<Movie> GetMovies(); 
+    }
+}
